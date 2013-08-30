d6f1df07d1321aebc1e1c53e0b6c9d4a282432e5
Jordan Halterman <jordan.halterman@gmail.com>
2013-08-30 12:21:26 -0700

    Add helper classes for publishing a single message to multiple channels.

---
+++ vine-core/src/main/java/com/blankstyle/vine/messaging/ChannelPublisher.java
@@ -0,0 +1,46 @@
+/*
+* Copyright 2013 the original author or authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package com.blankstyle.vine.messaging;
+
+import org.vertx.java.core.AsyncResult;
+import org.vertx.java.core.Handler;
+
+/**
+ * A channel publisher which publishes a single message to multiple channels.
+ *
+ * @author Jordan Halterman
+ */
+public interface ChannelPublisher {
+
+  /**
+   * Publishes a message.
+   *
+   * @param message
+   *   The message to publish.
+   */
+  public void publish(JsonMessage message);
+
+  /**
+   * Publishes a message.
+   *
+   * @param message
+   *   The message to publish.
+   * @param doneHandler
+   *   A handler to be invoked once the message has been received.
+   */
+  public void publish(JsonMessage message, Handler<AsyncResult<Void>> doneHandler);
+
+}
---
+++ vine-core/src/main/java/com/blankstyle/vine/messaging/RecursiveChannelPublisher.java
@@ -0,0 +1,113 @@
+/*
+* Copyright 2013 the original author or authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package com.blankstyle.vine.messaging;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.vertx.java.core.AsyncResult;
+import org.vertx.java.core.AsyncResultHandler;
+import org.vertx.java.core.Handler;
+import org.vertx.java.core.impl.DefaultFutureResult;
+
+/**
+ * Publishes messages to reliable channels, recursively awaiting responses
+ * before sending the next message.
+ *
+ * @author Jordan Halterman
+ */
+public class RecursiveChannelPublisher implements ChannelPublisher {
+
+  private List<ReliableChannel> channels = new ArrayList<ReliableChannel>();
+
+  /**
+   * Adds a channel to the publisher.
+   *
+   * @param channel
+   *   The channel to add.
+   * @return
+   *   The called publisher instance.
+   */
+  public RecursiveChannelPublisher addChannel(ReliableChannel channel) {
+    if (!channels.contains(channel)) {
+      channels.add(channel);
+    }
+    return this;
+  }
+
+  /**
+   * Removes a channel from the publisher.
+   *
+   * @param channel
+   *   The channel to remove.
+   * @return
+   *   The called publisher instance.
+   */
+  public RecursiveChannelPublisher removeChannel(ReliableChannel channel) {
+    if (channels.contains(channel)) {
+      channels.remove(channel);
+    }
+    return this;
+  }
+
+  @Override
+  public void publish(JsonMessage message) {
+    Iterator<ReliableChannel> iter = channels.iterator();
+    if (iter.hasNext()) {
+      recursivePublish(message, iter.next(), iter);
+    }
+  }
+
+  private void recursivePublish(final JsonMessage message, ReliableChannel currentChannel, final Iterator<ReliableChannel> iterator) {
+    currentChannel.publish(message, new AsyncResultHandler<Void>() {
+      @Override
+      public void handle(AsyncResult<Void> result) {
+        if (iterator.hasNext()) {
+          recursivePublish(message, iterator.next(), iterator);
+        }
+      }
+    });
+  }
+
+  @Override
+  public void publish(JsonMessage message, Handler<AsyncResult<Void>> doneHandler) {
+    Iterator<ReliableChannel> iter = channels.iterator();
+    if (iter.hasNext()) {
+      recursivePublish(message, iter.next(), iter, doneHandler);
+    }
+  }
+
+  private void recursivePublish(final JsonMessage message, ReliableChannel currentChannel, final Iterator<ReliableChannel> iterator, final Handler<AsyncResult<Void>> doneHandler) {
+    currentChannel.publish(message, new AsyncResultHandler<Void>() {
+      @Override
+      public void handle(AsyncResult<Void> result) {
+        if (result.succeeded()) {
+          if (iterator.hasNext()) {
+            recursivePublish(message, iterator.next(), iterator, doneHandler);
+          }
+          else {
+            new DefaultFutureResult<Void>().setHandler(doneHandler).setResult(null);
+          }
+        }
+        else {
+          new DefaultFutureResult<Void>().setHandler(doneHandler).setFailure(result.cause());
+        }
+      }
+    });
+  }
+
+}

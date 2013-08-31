523c74cc627d8e39018d0ce8aef4d7442a53ed4a
Jordan Halterman <jordan.halterman@gmail.com>
2013-08-30 21:17:28 -0700

    Add remote vine test.

--- vine-core/src/test/java/com/blankstyle/vine/test/integration/VineTest.java
+++ vine-core/src/test/java/com/blankstyle/vine/test/integration/VineTest.java
@@ -2,7 +2,9 @@ package com.blankstyle.vine.test.integration;
 
 import org.junit.Test;
 import org.vertx.java.core.AsyncResult;
+import org.vertx.java.core.Future;
 import org.vertx.java.core.Handler;
+import org.vertx.java.core.impl.DefaultFutureResult;
 import org.vertx.java.core.json.JsonObject;
 import org.vertx.testtools.TestVerticle;
 
@@ -11,7 +13,10 @@ import com.blankstyle.vine.Seeds;
 import com.blankstyle.vine.Vine;
 import com.blankstyle.vine.Vines;
 import com.blankstyle.vine.definition.VineDefinition;
+import com.blankstyle.vine.eventbus.root.RootVerticle;
+import com.blankstyle.vine.eventbus.stem.StemVerticle;
 import com.blankstyle.vine.local.LocalRoot;
+import com.blankstyle.vine.remote.RemoteRoot;
 
 import static org.vertx.testtools.VertxAssert.assertTrue;
 import static org.vertx.testtools.VertxAssert.assertEquals;
@@ -57,6 +62,86 @@ public class VineTest extends TestVerticle {
         }
         else {
           assertTrue("Failed to deploy vine. "+ result.cause(), false);
+        }
+      }
+    });
+  }
+
+  @Test
+  public void testRemoteDeploy() {
+    deployRoot(new Handler<AsyncResult<Void>>() {
+      @Override
+      public void handle(AsyncResult<Void> result) {
+        if (result.succeeded()) {
+          deployStem(new Handler<AsyncResult<Void>>() {
+            @Override
+            public void handle(AsyncResult<Void> result) {
+              if (result.succeeded()) {
+                VineDefinition vine = createTestDefinition();
+                RemoteRoot root = new RemoteRoot("vine.root.test", vertx, container);
+                root.deploy(vine, new Handler<AsyncResult<Vine>>() {
+                  @Override
+                  public void handle(AsyncResult<Vine> result) {
+                    if (result.succeeded()) {
+                      Vine vine = result.result();
+                      Feeder feeder = vine.feeder();
+                      feeder.feed(new JsonObject().putString("body", "Hello world!"), new Handler<AsyncResult<JsonObject>>() {
+                        @Override
+                        public void handle(AsyncResult<JsonObject> result) {
+                          if (result.succeeded()) {
+                            assertEquals("Hello world again again!", result.result().getString("body"));
+                          }
+                          else {
+                            assertTrue("Failed to process message.", false);
+                          }
+                          testComplete();
+                        }
+                      });
+                    }
+                    else {
+                      assertTrue("Failed to deploy vine. "+ result.cause(), false);
+                    }
+                  }
+                });
+              }
+            }
+          });
+        }
+      }
+    });
+  }
+
+  /**
+   * Deploys a test root.
+   */
+  private void deployRoot(Handler<AsyncResult<Void>> doneHandler) {
+    final Future<Void> future = new DefaultFutureResult<Void>().setHandler(doneHandler);
+    container.deployVerticle(RootVerticle.class.getName(), new JsonObject().putString("address", "vine.root.test"), new Handler<AsyncResult<String>>() {
+      @Override
+      public void handle(AsyncResult<String> result) {
+        if (result.succeeded()) {
+          future.setResult(null);
+        }
+        else {
+          assertTrue("Failed to deploy root.", false);
+        }
+      }
+    });
+  }
+
+  /**
+   * Deploys a test stem.
+   */
+  private void deployStem(Handler<AsyncResult<Void>> doneHandler) {
+    final Future<Void> future = new DefaultFutureResult<Void>().setHandler(doneHandler);
+    container.deployVerticle(StemVerticle.class.getName(), new JsonObject().putString("address", "vine.stem.test"), new Handler<AsyncResult<String>>() {
+      @Override
+      public void handle(AsyncResult<String> result) {
+        if (result.succeeded()) {
+          future.setResult(null);
+        }
+        else {
+          assertTrue("Failed to deploy stem.", false);
         }
       }
     });

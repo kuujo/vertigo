c247f2275fb899cfb5691467c038091082059266
Jordan Halterman <jordan.halterman@gmail.com>
2013-10-11 02:58:08 -0700

    Add basic executor implementations.

---
+++ vertigo-core/src/main/java/net/kuujo/vertigo/component/executor/AbstractExecutor.java
@@ -0,0 +1,160 @@
+/*
+* Copyright 2013 the original author or authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package net.kuujo.vertigo.component.executor;
+
+import org.vertx.java.core.AsyncResult;
+import org.vertx.java.core.Future;
+import org.vertx.java.core.Handler;
+import org.vertx.java.core.Vertx;
+import org.vertx.java.core.impl.DefaultFutureResult;
+import org.vertx.java.core.json.JsonObject;
+import org.vertx.java.platform.Container;
+
+import net.kuujo.vertigo.component.ComponentBase;
+import net.kuujo.vertigo.context.WorkerContext;
+import net.kuujo.vertigo.messaging.DefaultJsonMessage;
+import net.kuujo.vertigo.messaging.JsonMessage;
+
+/**
+ * An abstract executor implementation.
+ *
+ * @author Jordan Halterman
+ *
+ * @param <T> The executor type
+ */
+public abstract class AbstractExecutor<T extends Executor<T>> extends ComponentBase implements Executor<T> {
+
+  protected ExecuteQueue queue;
+
+  protected AbstractExecutor(Vertx vertx, Container container, WorkerContext context) {
+    super(vertx, container, context);
+    queue = new BasicExecuteQueue(vertx);
+  }
+
+  @Override
+  @SuppressWarnings("unchecked")
+  public T start() {
+    setupHeartbeat();
+    setupOutputs();
+    setupInputs();
+    ready();
+    return (T) this;
+  }
+
+  @Override
+  @SuppressWarnings("unchecked")
+  public T start(Handler<AsyncResult<T>> doneHandler) {
+    final Future<T> future = new DefaultFutureResult<T>().setHandler(doneHandler);
+    setupHeartbeat(new Handler<AsyncResult<Void>>() {
+      @Override
+      public void handle(AsyncResult<Void> result) {
+        if (result.failed()) {
+          future.setFailure(result.cause());
+        }
+        else {
+          setupOutputs(new Handler<AsyncResult<Void>>() {
+            @Override
+            public void handle(AsyncResult<Void> result) {
+              if (result.failed()) {
+                future.setFailure(result.cause());
+              }
+              else {
+                setupInputs(new Handler<AsyncResult<Void>>() {
+                  @Override
+                  public void handle(AsyncResult<Void> result) {
+                    if (result.failed()) {
+                      future.setFailure(result.cause());
+                    }
+                    else {
+                      ready(new Handler<AsyncResult<Void>>() {
+                        @Override
+                        public void handle(AsyncResult<Void> result) {
+                          if (result.failed()) {
+                            future.setFailure(result.cause());
+                          }
+                          else {
+                            future.setResult((T) AbstractExecutor.this);
+                          }
+                        }
+                      });
+                    }
+                  }
+                });
+              }
+            }
+          });
+        }
+      }
+    });
+    return (T) this;
+  }
+
+  @Override
+  @SuppressWarnings("unchecked")
+  public T replyTimeout(long timeout) {
+    queue.replyTimeout(timeout);
+    return (T) this;
+  }
+
+  @Override
+  public long replyTimeout() {
+    return queue.replyTimeout();
+  }
+
+  @Override
+  @SuppressWarnings("unchecked")
+  public T maxQueueSize(long maxSize) {
+    queue.maxQueueSize(maxSize);
+    return (T) this;
+  }
+
+  @Override
+  public long maxQueueSize() {
+    return queue.maxQueueSize();
+  }
+
+  @Override
+  public boolean queueFull() {
+    return queue.full();
+  }
+
+  /**
+   * Executes a feed.
+   */
+  @SuppressWarnings("unchecked")
+  protected T doExecute(final JsonObject data, final String tag, Handler<AsyncResult<JsonMessage>> handler) {
+    final JsonMessage message = DefaultJsonMessage.create(address, data, tag);
+    queue.enqueue(message.id(), handler);
+    output.emit(message);
+    return (T) this;
+  }
+
+  @Override
+  protected void doAck(String id) {
+    queue.ack(id);
+  }
+
+  @Override
+  protected void doFail(String id) {
+    queue.fail(id);
+  }
+
+  @Override
+  protected void doReceive(JsonMessage message) {
+    queue.receive(message);
+  }
+
+}
---
+++ vertigo-core/src/main/java/net/kuujo/vertigo/component/executor/ExecuteQueue.java
@@ -0,0 +1,118 @@
+/*
+* Copyright 2013 the original author or authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package net.kuujo.vertigo.component.executor;
+
+import net.kuujo.vertigo.messaging.JsonMessage;
+
+import org.vertx.java.core.AsyncResult;
+import org.vertx.java.core.Handler;
+
+/**
+ * An executor queue.
+ *
+ * @author Jordan Halterman
+ */
+public interface ExecuteQueue {
+
+  /**
+   * Sets the queue reply timeout.
+   *
+   * @param timeout
+   *   The reply timeout.
+   * @return
+   *   The called executor queue instance.
+   */
+  public ExecuteQueue replyTimeout(long timeout);
+
+  /**
+   * Gets the queue reply timeout.
+   *
+   * @return
+   *   The queue reply timeout.
+   */
+  public long replyTimeout();
+
+  /**
+   * Sets the maximum executor queue size.
+   *
+   * @param maxSize
+   *   A executor queue size.
+   * @return
+   *   The called executor queue instance.
+   */
+  public ExecuteQueue maxQueueSize(long maxSize);
+
+  /**
+   * Gets the maximum executor queue size.
+   *
+   * @return
+   *   The maximum allowed executor queue size.
+   */
+  public long maxQueueSize();
+
+  /**
+   * Indicates the current executor queue size.
+   *
+   * @return
+   *   The current feed executor size.
+   */
+  public long size();
+
+  /**
+   * Indicates whether the executor queue is full.
+   *
+   * @return
+   *   A boolean indicating whether the executor queue is full.
+   */
+  public boolean full();
+
+  /**
+   * Adds a unique ID to the queue.
+   *
+   * @param id
+   *   A unique message ID.
+   * @param resultHandler
+   *   A handler to be invoked once the response is received.
+   * @return
+   *   The called executor queue instance.
+   */
+  public ExecuteQueue enqueue(String id, Handler<AsyncResult<JsonMessage>> resultHandler);
+
+  /**
+   * Acks a message in the queue.
+   *
+   * @param id
+   *   The unique message ID.
+   */
+  public void ack(String id);
+
+  /**
+   * Fails a message in the queue.
+   *
+   * @param id
+   *   The unique message ID.
+   */
+  public void fail(String id);
+
+  /**
+   * Receives a response.
+   *
+   * @param message
+   *   The response message.
+   */
+  public void receive(JsonMessage message);
+
+}

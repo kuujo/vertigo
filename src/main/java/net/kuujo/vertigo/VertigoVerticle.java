6f57f4c60b8d16b5b75bc29c975c19c068f8acb1
Jordan Halterman <jordan.halterman@gmail.com>
2013-10-20 06:11:57 -0700

    Move VertigoVerticle implementation to a new package.

---
+++ src/main/java/net/kuujo/vertigo/VertigoVerticle.java
@@ -0,0 +1,57 @@
+/*
+* Copyright 2013 the original author or authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package net.kuujo.vertigo;
+
+import net.kuujo.vertigo.context.InstanceContext;
+
+import org.vertx.java.core.Future;
+import org.vertx.java.core.Vertx;
+import org.vertx.java.platform.Container;
+import org.vertx.java.platform.Verticle;
+
+/**
+ * A Vert.igo verticle implementation.
+ *
+ * The VertigoVerticle is an extension of the core Vert.x verticle that allows users
+ * access to a Vert.igo instance for creating feeders, workers, and executors.
+ *
+ * @author Jordan Halterman
+ */
+public class VertigoVerticle extends Verticle {
+
+  protected Vertigo vertigo;
+
+  protected InstanceContext context;
+
+  @Override
+  public void setContainer(Container container) {
+    super.setContainer(container);
+  }
+
+  @Override
+  public void setVertx(Vertx vertx) {
+    super.setVertx(vertx);
+  }
+
+  @Override
+  public void start(Future<Void> startedResult) {
+    vertigo = new DefaultVertigo(vertx, container);
+    context = InstanceContext.fromJson(container.config());
+    vertigo.setContext(context);
+    super.start(startedResult);
+  }
+
+}

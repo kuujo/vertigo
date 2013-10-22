c575ccc709c1f7eb5bb85a21310f2f5028383324
Jordan Halterman <jordan.halterman@gmail.com>
2013-10-21 17:41:06 -0700

    Add unique IDs to groupings.

---
+++ src/main/java/net/kuujo/vertigo/input/grouping/AbstractGrouping.java
@@ -0,0 +1,35 @@
+package net.kuujo.vertigo.input.grouping;
+
+import java.util.UUID;
+
+import org.vertx.java.core.json.JsonObject;
+
+/**
+ * An abstract grouping implementation.
+ *
+ * @author Jordan Halterman
+ */
+public abstract class AbstractGrouping implements Grouping {
+
+  private JsonObject definition = new JsonObject();
+
+  public AbstractGrouping() {
+    definition.putString("id", UUID.randomUUID().toString());
+  }
+
+  @Override
+  public String id() {
+    return definition.getString("id");
+  }
+
+  @Override
+  public JsonObject getState() {
+    return definition.copy();
+  }
+
+  @Override
+  public void setState(JsonObject state) {
+    definition = state.copy();
+  }
+
+}

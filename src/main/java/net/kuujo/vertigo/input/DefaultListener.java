375391c3371b5282ddb311aca75b4841d5ea9bc2
Jordan Halterman <jordan.halterman@gmail.com>
2013-10-21 18:06:59 -0700

    Add licensing documentation.

--- src/main/java/net/kuujo/vertigo/input/DefaultListener.java
+++ src/main/java/net/kuujo/vertigo/input/DefaultListener.java
@@ -1,3 +1,18 @@
+/*
+* Copyright 2013 the original author or authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 package net.kuujo.vertigo.input;
 
 import java.util.UUID;
@@ -62,17 +77,33 @@ public class DefaultListener implements Listener {
     public void handle(Message<JsonObject> message) {
       JsonObject body = message.body();
       if (body != null) {
-        if (messageHandler != null) {
-          try {
-            messageHandler.handle((JsonMessage) Serializer.deserialize(body));
-          }
-          catch (SerializationException e) {
-            // Do nothing.
-          }
-        }
+        doReceive(body);
       }
     }
   };
+
+  /**
+   * Receives message data.
+   */
+  private void doReceive(JsonObject messageData) {
+    try {
+      JsonMessage message = (JsonMessage) Serializer.deserialize(messageData);
+      String auditor = message.auditor();
+ 
+      // Call the message handler.
+      if (messageHandler != null) {
+        messageHandler.handle(message);
+      }
+
+      // Always attempt to ack the message once the handler has been called.
+      if (auditor != null) {
+        eventBus.send(auditor, new JsonObject().putString("action", "ack").putString("id", message.id()));
+      }
+    }
+    catch (SerializationException e) {
+      // Do nothing.
+    }
+  }
 
   @Override
   public Listener messageHandler(Handler<JsonMessage> handler) {

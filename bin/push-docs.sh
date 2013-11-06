0e1e0b5cbb401bc9edf0cf4f3400899850d1010f
Jordan Halterman <jordan.halterman@gmail.com>
2013-11-05 16:55:20 -0800

    Add shell script for updating/pushing javadocs.

---
+++ bin/push-docs.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+rm -rf docs
+git clone git@github.com:kuujo/vertigo.git docs -b gh-pages
+mvn -Pjavadoc javadoc:javadoc
+cd docs
+git add -A
+git commit -m "Updated documentation."
+git push origin gh-pages

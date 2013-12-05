62c0db36cf7be1e7e319f1b705adcaab8f445e58
Jordan Halterman <jordan.halterman@gmail.com>
2013-12-05 04:39:15 -0800

    Add rich Java verticle implementations.

---
+++ src/main/java/net/kuujo/vertigo/java/RichExecutorVerticle.java
@@ -0,0 +1,160 @@
+/*
+ * Copyright 2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.kuujo.vertigo.java;
+
+import org.vertx.java.core.AsyncResult;
+import org.vertx.java.core.Handler;
+import org.vertx.java.core.json.JsonObject;
+
+import net.kuujo.vertigo.component.ComponentFactory;
+import net.kuujo.vertigo.component.DefaultComponentFactory;
+import net.kuujo.vertigo.context.InstanceContext;
+import net.kuujo.vertigo.message.JsonMessage;
+import net.kuujo.vertigo.message.MessageId;
+import net.kuujo.vertigo.rpc.Executor;
+import net.kuujo.vertigo.runtime.FailureException;
+import net.kuujo.vertigo.runtime.TimeoutException;
+
+/**
+ * A rich executor verticle implementation.
+ *
+ * @author Jordan Halterman
+ */
+public abstract class RichExecutorVerticle extends ComponentVerticle<Executor> {
+  protected Executor executor;
+  protected long replyTimeout = 30000;
+  protected long executeQueueMaxSize = 1000;
+  protected boolean autoRetry = false;
+  protected int autoRetryAttempts = -1;
+  protected long executeInterval = 10;
+
+  @Override
+  protected Executor createComponent(InstanceContext<Executor> context) {
+    ComponentFactory componentFactory = new DefaultComponentFactory(vertx, container);
+    return componentFactory.createExecutor(context);
+  }
+
+  @Override
+  protected void start(Executor executor) {
+    this.executor = executor;
+    executor.setReplyTimeout(replyTimeout)
+      .setExecuteQueueMaxSize(executeQueueMaxSize)
+      .setAutoRetry(autoRetry)
+      .setAutoRetryAttempts(autoRetryAttempts)
+      .setExecuteInterval(executeInterval);
+    executor.executeHandler(new Handler<Executor>() {
+      @Override
+      public void handle(Executor executor) {
+        nextMessage();
+      }
+    });
+  }
+
+  private Handler<AsyncResult<JsonMessage>> wrapHandler(final Handler<AsyncResult<JsonMessage>> resultHandler) {
+    return new Handler<AsyncResult<JsonMessage>>() {
+      @Override
+      public void handle(AsyncResult<JsonMessage> result) {
+        if (result.failed()) {
+          if (result.cause() instanceof FailureException) {
+            handleFailure((FailureException) result.cause());
+          }
+          else if (result.cause() instanceof TimeoutException) {
+            handleTimeout((TimeoutException) result.cause());
+          }
+        }
+        else {
+          handleResult(result.result());
+        }
+        if (resultHandler != null) {
+          resultHandler.handle(result);
+        }
+      }
+    };
+  }
+
+  /**
+   * Called when the executor is requesting the next message.
+   *
+   * Override this method to perform polling-based executions. The executor will automatically
+   * call this method any time the execute queue is prepared to accept new messages.
+   */
+  protected void nextMessage() {
+  }
+
+  /**
+   * Indicates whether the execute queue is full.
+   *
+   * @return
+   *   Indicates whether the queue is full.
+   */
+  public boolean executeQueueFull() {
+    return executor.executeQueueFull();
+  }
+
+  /**
+   * Executes a message.
+   *
+   * @param args
+   *   The output message body.
+   * @return
+   *   The output message identifier.
+   */
+  public MessageId execute(JsonObject args) {
+    return executor.execute(args, wrapHandler(null));
+  }
+
+  /**
+   * Executes a message.
+   *
+   * @param args
+   *   The output message body.
+   * @param resultHandler
+   *   An asynchronous handler to be called with the execution result.
+   * @return
+   *   The output message identifier.
+   */
+  public MessageId execute(JsonObject args, Handler<AsyncResult<JsonMessage>> resultHandler) {
+    return executor.execute(args, wrapHandler(resultHandler));
+  }
+
+  /**
+   * Called when a message is successfully processed and a result is received.
+   *
+   * Override this method to provide custom handling for execution results.
+   *
+   * @param result
+   *   The result message.
+   */
+  protected void handleResult(JsonMessage result) {
+  }
+
+  /**
+   * Called when a message is explicitly failed.
+   *
+   * Override this method to provide custom handling for message failures.
+   */
+  protected void handleFailure(FailureException cause) {
+  }
+
+  /**
+   * Called when a message times out.
+   *
+   * Override this method to provide custom handling for message timeouts.
+   */
+  protected void handleTimeout(TimeoutException cause) {
+  }
+
+}
---
+++ src/main/java/net/kuujo/vertigo/java/RichWorkerVerticle.java
@@ -0,0 +1,119 @@
+/*
+ * Copyright 2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.kuujo.vertigo.java;
+
+import org.vertx.java.core.Handler;
+import org.vertx.java.core.json.JsonObject;
+
+import net.kuujo.vertigo.component.ComponentFactory;
+import net.kuujo.vertigo.component.DefaultComponentFactory;
+import net.kuujo.vertigo.context.InstanceContext;
+import net.kuujo.vertigo.message.JsonMessage;
+import net.kuujo.vertigo.message.MessageId;
+import net.kuujo.vertigo.worker.Worker;
+
+/**
+ * A rich worker verticle implementation.
+ *
+ * @author Jordan Halterman
+ */
+public abstract class RichWorkerVerticle extends ComponentVerticle<Worker> {
+  protected Worker worker;
+
+  @Override
+  protected Worker createComponent(InstanceContext<Worker> context) {
+    ComponentFactory componentFactory = new DefaultComponentFactory(vertx, container);
+    return componentFactory.createWorker(context);
+  }
+
+  @Override
+  protected void start(final Worker worker) {
+    this.worker = worker;
+    worker.messageHandler(new Handler<JsonMessage>() {
+      @Override
+      public void handle(JsonMessage message) {
+        handleMessage(message);
+      }
+    });
+  }
+
+  /**
+   * Called when a new message is received by the worker.
+   *
+   * @param message
+   *   The message that was received.
+   */
+  protected abstract void handleMessage(JsonMessage message);
+
+  /**
+   * Emits data from the worker.
+   *
+   * @param data
+   *   The data to emit.
+   * @return
+   *   The emitted message identifier.
+   */
+  public MessageId emit(JsonObject data) {
+    return worker.emit(data);
+  }
+
+  /**
+   * Emits data as the child of a message.
+   *
+   * @param data
+   *   The data to emit.
+   * @param parent
+   *   The parent message.
+   * @return
+   *   The emitted message identifier.
+   */
+  public MessageId emit(JsonObject data, JsonMessage parent) {
+    return worker.emit(data, parent);
+  }
+
+  /**
+   * Emits a message as a child of itself.
+   *
+   * @param message
+   *   The message to emit.
+   * @return
+   *   The emitted message identifier.
+   */
+  public MessageId emit(JsonMessage message) {
+    return worker.emit(message);
+  }
+
+  /**
+   * Acks a message.
+   *
+   * @param message
+   *   The message to ack.
+   */
+  public void ack(JsonMessage message) {
+    worker.ack(message);
+  }
+
+  /**
+   * Fails a message.
+   *
+   * @param message
+   *   The message to fail.
+   */
+  public void fail(JsonMessage message) {
+    worker.fail(message);
+  }
+
+}

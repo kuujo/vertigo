b82c1a6e09b98536e53b64e7b7ae69580baf266e
Jordan Halterman <jordan.halterman@gmail.com>
2013-12-09 14:17:51 -0800

    Add additional integration tests for emitting to specific streams and nested networks.

--- src/test/java/net/kuujo/vertigo/test/integration/NetworkTest.java
+++ src/test/java/net/kuujo/vertigo/test/integration/NetworkTest.java
@@ -17,24 +17,33 @@ package net.kuujo.vertigo.test.integration;
 
 import net.kuujo.vertigo.cluster.Cluster;
 import net.kuujo.vertigo.cluster.LocalCluster;
+import net.kuujo.vertigo.java.FeederVerticle;
+import net.kuujo.vertigo.java.WorkerVerticle;
+import net.kuujo.vertigo.message.JsonMessage;
+import net.kuujo.vertigo.message.MessageId;
 import net.kuujo.vertigo.network.Network;
 import net.kuujo.vertigo.context.NetworkContext;
+import net.kuujo.vertigo.feeder.Feeder;
 import net.kuujo.vertigo.testtools.TestAckingFeeder;
 import net.kuujo.vertigo.testtools.TestAckingWorker;
 import net.kuujo.vertigo.testtools.TestFailingExecutor;
 import net.kuujo.vertigo.testtools.TestFailingFeeder;
 import net.kuujo.vertigo.testtools.TestFailingWorker;
+import net.kuujo.vertigo.testtools.TestPeriodicFeeder;
 import net.kuujo.vertigo.testtools.TestResultCheckingExecutor;
 import net.kuujo.vertigo.testtools.TestTimingOutExecutor;
 import net.kuujo.vertigo.testtools.TestTimingOutFeeder;
 import net.kuujo.vertigo.testtools.TestTimingOutWorker;
+import net.kuujo.vertigo.worker.Worker;
 
 import org.junit.Test;
 import org.vertx.java.core.AsyncResult;
 import org.vertx.java.core.Handler;
+import org.vertx.java.core.json.JsonArray;
 import org.vertx.java.core.json.JsonObject;
 
 import static org.vertx.testtools.VertxAssert.assertTrue;
+import static org.vertx.testtools.VertxAssert.testComplete;
 
 import org.vertx.testtools.TestVerticle;
 
@@ -50,18 +59,7 @@ public class NetworkTest extends TestVerticle {
     Network network = new Network("test");
     network.addFeeder("feeder", TestAckingFeeder.class.getName(), new JsonObject().putString("body", "Hello world"));
     network.addWorker("worker", TestAckingWorker.class.getName()).addInput("feeder");
-    Cluster cluster = new LocalCluster(vertx, container);
-    cluster.deploy(network, new Handler<AsyncResult<NetworkContext>>() {
-      @Override
-      public void handle(AsyncResult<NetworkContext> result) {
-        if (result.failed()) {
-          assertTrue(result.cause().getMessage(), result.succeeded());
-        }
-        else {
-          assertTrue(result.succeeded());
-        }
-      }
-    });
+    deployNetwork(network);
   }
 
   @Test
@@ -69,18 +67,7 @@ public class NetworkTest extends TestVerticle {
     Network network = new Network("test");
     network.addFeeder("feeder", TestFailingFeeder.class.getName(), new JsonObject().putString("body", "Hello world"));
     network.addWorker("worker", TestFailingWorker.class.getName()).addInput("feeder");
-    Cluster cluster = new LocalCluster(vertx, container);
-    cluster.deploy(network, new Handler<AsyncResult<NetworkContext>>() {
-      @Override
-      public void handle(AsyncResult<NetworkContext> result) {
-        if (result.failed()) {
-          assertTrue(result.cause().getMessage(), result.succeeded());
-        }
-        else {
-          assertTrue(result.succeeded());
-        }
-      }
-    });
+    deployNetwork(network);
   }
 
   @Test
@@ -89,18 +76,28 @@ public class NetworkTest extends TestVerticle {
     network.setAckTimeout(1000);
     network.addFeeder("feeder", TestTimingOutFeeder.class.getName(), new JsonObject().putString("body", "Hello world"));
     network.addWorker("worker", TestTimingOutWorker.class.getName()).addInput("feeder");
-    Cluster cluster = new LocalCluster(vertx, container);
-    cluster.deploy(network, new Handler<AsyncResult<NetworkContext>>() {
-      @Override
-      public void handle(AsyncResult<NetworkContext> result) {
-        if (result.failed()) {
-          assertTrue(result.cause().getMessage(), result.succeeded());
-        }
-        else {
+    deployNetwork(network);
+  }
+
+  public static class StreamFeeder extends FeederVerticle {
+    @Override
+    public void start(Feeder feeder) {
+      feeder.emit("stream", new JsonObject().putString("body", "Hello world!"), new Handler<AsyncResult<MessageId>>() {
+        @Override
+        public void handle(AsyncResult<MessageId> result) {
           assertTrue(result.succeeded());
+          testComplete();
         }
-      }
-    });
+      });
+    }
+  }
+
+  @Test
+  public void testStreamFeeder() {
+    Network network = new Network("test");
+    network.addFeeder("feeder", StreamFeeder.class.getName());
+    network.addWorker("worker", TestAckingWorker.class.getName()).addInput("feeder", "stream");
+    deployNetwork(network);
   }
 
   @Test
@@ -109,18 +106,7 @@ public class NetworkTest extends TestVerticle {
     JsonObject data = new JsonObject().putString("body", "Hello world!");
     network.addExecutor("executor", TestResultCheckingExecutor.class.getName(), new JsonObject().putObject("input", data).putObject("output", data)).addInput("worker");
     network.addWorker("worker", TestAckingWorker.class.getName()).addInput("executor");
-    Cluster cluster = new LocalCluster(vertx, container);
-    cluster.deploy(network, new Handler<AsyncResult<NetworkContext>>() {
-      @Override
-      public void handle(AsyncResult<NetworkContext> result) {
-        if (result.failed()) {
-          assertTrue(result.cause().getMessage(), result.succeeded());
-        }
-        else {
-          assertTrue(result.succeeded());
-        }
-      }
-    });
+    deployNetwork(network);
   }
 
   @Test
@@ -129,18 +115,7 @@ public class NetworkTest extends TestVerticle {
     JsonObject data = new JsonObject().putString("body", "Hello world!");
     network.addExecutor("executor", TestFailingExecutor.class.getName(), new JsonObject().putObject("input", data).putObject("output", data)).addInput("worker");
     network.addWorker("worker", TestFailingWorker.class.getName()).addInput("executor");
-    Cluster cluster = new LocalCluster(vertx, container);
-    cluster.deploy(network, new Handler<AsyncResult<NetworkContext>>() {
-      @Override
-      public void handle(AsyncResult<NetworkContext> result) {
-        if (result.failed()) {
-          assertTrue(result.cause().getMessage(), result.succeeded());
-        }
-        else {
-          assertTrue(result.succeeded());
-        }
-      }
-    });
+    deployNetwork(network);
   }
 
   @Test
@@ -150,6 +125,43 @@ public class NetworkTest extends TestVerticle {
     JsonObject data = new JsonObject().putString("body", "Hello world!");
     network.addExecutor("executor", TestTimingOutExecutor.class.getName(), new JsonObject().putObject("input", data).putObject("output", data)).addInput("worker");
     network.addWorker("worker", TestTimingOutWorker.class.getName()).addInput("executor");
+    deployNetwork(network);
+  }
+
+  public static class NestedWorker extends WorkerVerticle {
+    @Override
+    protected void handleMessage(JsonMessage message, Worker worker) {
+      testComplete();
+    }
+  }
+
+  @Test
+  public void testNestedNetwork() {
+    Network network1 = new Network("network1");
+    network1.addFeeder("network1.feeder1", TestPeriodicFeeder.class.getName(), new JsonObject().putArray("fields", new JsonArray().add("body")));
+    network1.addWorker("network1.worker1", TestAckingWorker.class.getName(), 2).addInput("network1.feeder1").randomGrouping();
+    network1.addWorker("network1.worker2", TestAckingWorker.class.getName(), 2).addInput("network1.worker1").roundGrouping();
+
+    final Cluster cluster = new LocalCluster(vertx, container);
+    cluster.deploy(network1, new Handler<AsyncResult<NetworkContext>>() {
+      @Override
+      public void handle(AsyncResult<NetworkContext> result) {
+        if (result.failed()) {
+          assertTrue(result.cause().getMessage(), result.succeeded());
+        }
+        else {
+          assertTrue(result.succeeded());
+
+          Network network2 = new Network("network2");
+          network2.addWorker("network2.worker1", TestAckingWorker.class.getName(), 2).addInput("network1.worker2").randomGrouping();
+          network2.addWorker("network2.worker2", NestedWorker.class.getName(), 2).addInput("network2.worker1").roundGrouping();
+          deployNetwork(network2);
+        }
+      }
+    });
+  }
+
+  private void deployNetwork(Network network) {
     Cluster cluster = new LocalCluster(vertx, container);
     cluster.deploy(network, new Handler<AsyncResult<NetworkContext>>() {
       @Override

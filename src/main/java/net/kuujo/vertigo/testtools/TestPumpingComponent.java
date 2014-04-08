496576f71c1bc97c438c528ac2f5341d1e14c45b
Jordan Halterman <jordan.halterman@gmail.com>
2014-04-08 01:16:20 -0700

    Add test pumping component.

---
+++ src/main/java/net/kuujo/vertigo/testtools/TestPumpingComponent.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.kuujo.vertigo.testtools;
+
+import net.kuujo.vertigo.component.Component;
+import net.kuujo.vertigo.java.ComponentVerticle;
+import net.kuujo.vertigo.message.JsonMessage;
+
+import org.vertx.java.core.Handler;
+
+/**
+ * Pumping test component.
+ *
+ * @author Jordan Halterman
+ */
+public class TestPumpingComponent extends ComponentVerticle {
+
+  @Override
+  public void start(final Component component) {
+    component.input().port("in").messageHandler(new Handler<JsonMessage>() {
+      @Override
+      public void handle(JsonMessage message) {
+        component.output().port("out").send(message.body());
+      }
+    });
+  }
+
+}

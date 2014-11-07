c1915e8f4697341011e4b8e5fc1554f43c7c13fd
Jordan Halterman <jordan.halterman@gmail.com>
2014-11-06 20:20:07 -0800

    Add network/component reference types for operating on running networks and components.

---
+++ core/src/main/java/net/kuujo/vertigo/io/OutputReference.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.kuujo.vertigo.io;
+
+import net.kuujo.vertigo.io.port.OutputPortReference;
+
+/**
+ * Reference to component output.
+ *
+ * @author <a href="http://github.com/kuujo">Jordan Halterman</a>
+ */
+public interface OutputReference {
+
+  /**
+   * Returns a reference to an output port.
+   *
+   * @param name The name of the output port to reference.
+   * @param <T> The output port type.
+   * @return The output port reference.
+   */
+  <T> OutputPortReference<T> port(String name);
+
+}
